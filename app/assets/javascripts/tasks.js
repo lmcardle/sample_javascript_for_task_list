c90fdd1d0bf265534e8982a88307f600a054ed9e
Liam McArdle <liam@catholicsteward.com>
2012-10-09 22:05:55 -0700

    add comments to show coffeescript version

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -10,3 +10,13 @@ $(function() {
 	$('.edit_task').submitOnCheck();
 });
 
+// CoffeeScript Version:
+// 
+// jQuery.fn.submitOnCheck = ->
+// 	@find('input[type=submit]').remove()
+// 	@find('input[type=checkbox]').click ->
+// 		$(this).parent('form').submit()
+// 	this
+// 	
+// jQuery ->
+// 	$('.edit_task').submitOnCheck()
